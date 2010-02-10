9f162ae926530030c29e0668c13f3d5003776cb1
Marcel Grünauer <marcel@cpan.org>
2010-02-10 09:56:15 +0000

    Import of MARCEL/Hash-Rename-0.03 from CPAN.
    gitpan-cpan-distribution: Hash-Rename
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MARCEL/Hash-Rename-0.03.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Hash/Rename.pm
+++ lib/Hash/Rename.pm
@@ -1,51 +1,33 @@
 package Hash::Rename;
-
+use 5.006;
 use strict;
 use warnings;
-
-
-our $VERSION = '0.01';
-
-
-use base 'Exporter';
-
-
+our $VERSION = '0.03';
+use Exporter qw(import);
 our @EXPORT = ('hash_rename');
-
 
 sub hash_rename (\%@) {
     my ($hash, %args) = @_;
-
     my %new_hash;
-
     for my $key (sort keys %$hash) {
         my $orig_key = $key;
         $key = $args{prepend} . $key if defined $args{prepend};
         $key = $key . $args{append}  if defined $args{append};
-
         if (defined $args{code}) {
-            ref $args{code} eq 'CODE' ||
-                die "'code' value is not a CODE reference";
+            ref $args{code} eq 'CODE'
+              || die "'code' value is not a CODE reference";
             local $_ = $key;
             $args{code}->();
             $key = $_;
         }
-
         die "duplicate result key [$key] from original key [$orig_key]\n"
-            if defined($args{strict}) && exists $new_hash{$key};
+          if defined($args{strict}) && exists $new_hash{$key};
         $new_hash{$key} = $hash->{$orig_key};
     }
-
     %$hash = %new_hash;
 }
-
-
 1;
-
-
 __END__
-
-
 
 =head1 NAME
 
@@ -69,7 +51,7 @@ Using this module you can rename a hash's keys in place.
 
 =over 4
 
-=item hash_rename.
+=item C<hash_rename>
 
 This function is automatically exported. It takes a hash to rename and another
 hash of instructions on how to rename they keys.
@@ -82,26 +64,26 @@ The following instructions are supported:
 
 =over 4
 
-=item prepend
+=item C<prepend>
 
     hash_rename %hash, prepend => '-';
 
 The given value is prepended to each hash key.
 
-=item append
+=item C<append>
 
     hash_rename %hash, append => '-';
 
 The given value is appended to each hash key.
 
-=item code
+=item C<code>
 
     hash_rename %hash, code => sub { s/^(?!-)/-/ };
 
 Each hash key is localized to C<$_> and subjected to the code. Its new value
 is the result of C<$_> after the code has been executed.
 
-=item strict
+=item C<strict>
 
 If present and set to a true value, the resulting keys are checked for
 duplicates. C<hash_rename()> will die if it detects a duplicate resulting hash
@@ -109,28 +91,18 @@ key. They keys of the hash to change are processed in alphabetical order.
 
 =back
 
-If several instructions are given, they are procesed in the order in which
+If several instructions are given, they are processed in the order in which
 they are described above. So you can have:
 
     hash_rename %hash, prepend => '-', append => '=';
 
 =back
 
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<hashrename> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.01 of L<Hash::Rename>.
-
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<<bug-hash-rename@rt.cpan.org>>, or through the web interface at
+Please report any bugs or feature requests through the web interface at
 L<http://rt.cpan.org>.
 
 =head1 INSTALLATION
@@ -141,19 +113,18 @@ See perlmodinstall for information and options on installing Perl modules.
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+site near you. Or see L<http://search.cpan.org/dist/Hash-Rename/>.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+Copyright 2007-2009 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
-
 
 =cut
 
--- t/01_misc.t
+++ t/01_misc.t
@@ -1,11 +1,9 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
 use Hash::Rename;
 use Test::More tests => 6;
 use Test::Exception;
-
 
 sub test_rename {
     my %args = @_;
@@ -16,38 +14,28 @@ sub test_rename {
     hash_rename %hash, %args;
     wantarray ? %hash : \%hash;
 }
-
-
 is_deeply(
     scalar(test_rename(prepend => '-')),
     { '--noforce' => 1, '-scheme' => 'http' },
     'prepend dash',
 );
-
 is_deeply(
     scalar(test_rename(append => '=')),
     { '-noforce=' => 1, 'scheme=' => 'http' },
     'append equal sign',
 );
-
 is_deeply(
     scalar(test_rename(prepend => '-', append => '=')),
     { '--noforce=' => 1, '-scheme=' => 'http' },
     'prepend and append',
 );
-
-is_deeply(
-    scalar(test_rename(code => sub { s/^(?!-)/-/ })),
-    { '-noforce' => 1, '-scheme' => 'http' },
-    'code',
-);
-
+is_deeply(scalar(test_rename(code => sub { s/^(?!-)/-/ })),
+    { '-noforce' => 1, '-scheme' => 'http' }, 'code',);
 is_deeply(
     scalar(test_rename(code => sub { $_ = 'foo' })),
     { foo => 'http' },
     'code producing duplicates, no strict',
 );
-
 throws_ok {
     is_deeply(
         scalar(test_rename(strict => 1, code => sub { $_ = 'foo' })),
@@ -55,6 +43,5 @@ throws_ok {
         'code producing duplicates, no strict',
     );
 }
-    qr/duplicate result key \[foo\] from original key \[scheme\]/,
-    'code producing duplicates, with strict',
-
+qr/duplicate result key \[foo\] from original key \[scheme\]/,
+  'code producing duplicates, with strict',
